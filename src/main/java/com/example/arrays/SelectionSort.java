53de6ca09a405e828b7a2085842e553770ab431b
Amit Raj <amit.894@gmail.com>
2022-03-13 21:19:43 +0530

    Adding Selection Sort Algo

---
+++ src/main/java/com/example/arrays/SelectionSort.java
@@ -0,0 +1,30 @@
+package com.example.arrays;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+
+    private static void selection_sort(int [] A){
+        for( int i=0;i<A.length;i++){
+            int min_idx=i;
+            for (int j=i;j<A.length;j++){
+                if (A[j]<=A[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp=A[i];
+            A[i]=A[min_idx];
+            A[min_idx]=temp;
+        }
+
+    }
+    public static void main(String args[]){
+        int [] A={2345, 123, 1, 678};
+        selection_sort(A);
+        System.out.println(Arrays.toString(A));
+    }
+}
+
+//Time Complexity : O(n^2)
+//Auxililary Space Complexity: O(1)
