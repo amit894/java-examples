c0664587ef46c42f97e6bf2ba6cbd457b8f5ea01
Amit Raj <amit.894@gmail.com>
2022-03-15 19:46:39 +0530

    Further optimizations

--- src/main/java/com/example/arrays/LeftRotateOptimizied.java
+++ src/main/java/com/example/arrays/LeftRotateOptimizied.java
@@ -6,43 +6,44 @@ public class LeftRotateOptimizied {
 
     public static void main (String args[]){
         int A[]={1,2,3,4,5};
-        left_optimizied(A,2,5);
+        left_optimizied(A,2);
         System.out.println(Arrays.toString(A));
     }
 
-    private static int gcd (int num1, int num2){
-        if (num1==0){
-            return num2;
-        }
-        if (num2==0){
-            return num1;
-        }
-        else {
-            return gcd(num2,num1%num2);
-        }
-    }
+  private static int gcd(int a, int b)
+  {
+      if(a==0){
+          return b;
+      }
+      else if (b==0){
+          return a;
+      }
+      else {
+          return gcd(b, a%b);
+      }
+  }
 
-    private static void left_optimizied(int A[], int d , int n){
-        d=d%n;
-        int g_c_d=gcd(1,5);
-        for (int i =0; i<g_c_d;i++){
-            int j =i;
+  private static void left_optimizied(int []A, int d){
+        d=d%A.length;
+        int g_c_d=gcd(d,A.length);
+        for (int i=0; i<g_c_d; i++){
+            int j=i;
             int temp=A[i];
-            while(true) {
-                int k = j+d;
-                if (k>=n) {
-                    k=k-n;
+            while (true){
+                int k=d+j;
+                if (k>=A.length){
+                    k=k-A.length;
                 }
                 if (k==i){
                     break;
                 }
                 A[j]=A[k];
                 j=k;
-
             }
             A[j]=temp;
         }
-    }
+
+  }
 }
 
 //Time Complexity : O(n)
