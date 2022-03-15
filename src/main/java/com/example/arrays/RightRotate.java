a8f1f3e540066080451e6c1a700921e43cfb6855
Amit Raj <amit.894@gmail.com>
2022-03-15 19:38:15 +0530

    Optimise Rotation

--- src/main/java/com/example/arrays/RightRotate.java
+++ src/main/java/com/example/arrays/RightRotate.java
@@ -4,25 +4,25 @@ import java.util.Arrays;
 
 public class RightRotate {
 
-    private static void right_rotate_by_one(int A[], int n){
-        int temp=A[n-1];
-        for( int i=n-1; i>0; i--){
-            A[i]=A[i-1];
-        }
-        A[0]=temp;
+      private static void right_rotate(int []A, int d){
+          for (int i=0;i<d;i++){
+              right_rotate_by_one(A);
+          }
 
-    }
+      }
 
-    private static void right_rotate(int A[], int n, int d){
-        for( int i=0; i<d; i++){
-            right_rotate_by_one(A,n);
-        }
-    }
+      private static void right_rotate_by_one(int []A){
+          int temp=A[A.length-1];
+          for (int i=A.length-1;i>0;i--){
+              A[i]=A[i-1];
+          }
+          A[0]=temp;
+      }
 
     public static void main (String args[]){
         int A[]={1,2,3,4,5};
         System.out.println("Hello World");
-        right_rotate(A,5,1);
+        right_rotate(A,1);
         System.out.println(Arrays.toString(A));
     }
 }
