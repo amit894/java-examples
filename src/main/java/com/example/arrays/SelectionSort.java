41ddb508f340d2e063555962dd314fe9eb66d926
Amit Raj <amit.894@gmail.com>
2022-03-28 10:18:06 +0530

    Basic Sorting Revision

--- src/main/java/com/example/arrays/SelectionSort.java
+++ src/main/java/com/example/arrays/SelectionSort.java
@@ -5,22 +5,22 @@ import java.util.Arrays;
 public class SelectionSort {
 
   private static void selection_sort(int [] A){
-      for (int i=0;i<A.length;i++){
-          int min_idx=i;
-          for ( int j=i;j<A.length;j++){
-              if ( A[j]<A[min_idx]){
-                  min_idx=j;
-              }
-          }
-          int temp=A[i];
-          A[i]=A[min_idx];
-          A[min_idx]=temp;
-      }
+    for (int i=0; i<A.length;i++){
+        int min_idx=i;
+        for ( int j=i;j<A.length;j++){
+            if ( A[j]<A[min_idx]){
+                min_idx=j;
+            }
+        }
+        int temp=A[min_idx];
+        A[min_idx]=A[i];
+        A[i]=temp;
+    }
   }
 
 
     public static void main(String args[]){
-        int [] A={-2345, -123, -1, -678};
+        int [] A={-2345,234, -123, -1, -678,0};
         selection_sort(A);
         System.out.println(Arrays.toString(A));
     }
