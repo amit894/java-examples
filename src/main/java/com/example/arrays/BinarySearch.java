f5ef2d37b2d3ef61d1ff32da6042839778f2ab4b
Amit Raj <amit.894@gmail.com>
2022-03-29 20:42:21 +0530

    Binary Search

---
+++ src/main/java/com/example/arrays/BinarySearch.java
@@ -0,0 +1,28 @@
+package com.example.arrays;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    private static int search (int low, int high, int [] A, int item){
+        int mid = (int) Math.floor((low + high) / 2);
+        if (low<=high) {
+            if (A[mid] == item) {
+                return mid;
+            } else if (A[mid] < item) {
+                return search(mid + 1, high, A, item);
+            } else {
+                return search(low, mid - 1, A, item);
+            }
+        }
+        return -1;
+
+    }
+    public static void main (String args []){
+        int [] A={1,2,3,4,5};
+        int index= search(0,A.length-1,A,5);
+        System.out.println(index);
+    }
+}
+
+// The time complexity of the binary search algorithm is O(log n)
