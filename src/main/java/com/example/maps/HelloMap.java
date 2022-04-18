eb70e24c33b06355784a2509588b75b28b6473be
Amit Raj <amit.894@gmail.com>
2022-04-18 12:15:28 +0530

    Some new algos

--- src/main/java/com/example/maps/HelloMap.java
+++ src/main/java/com/example/maps/HelloMap.java
@@ -1,41 +1,45 @@
 package com.example.maps;
+import javax.swing.text.MaskFormatter;
 import java.util.*;
 
 public class HelloMap {
 
     private static void string_string_map(){
-      HashMap <String,String> s1 = new HashMap<>();
-      s1.put("Amit","Devops");
-      s1.put("Gaurav","Developer");
-      s1.put("Vinod","QA");
+        HashMap <String,String> s1 = new HashMap<>();
+        s1.put("amit","key");
+        s1.put("dad","lock");
 
-      for (Map.Entry<String,String> entry: s1.entrySet()){
-          System.out.println(entry.getValue()+ ""+ entry.getKey());
-      }
+        System.out.println(s1);
+
+
+        for (Map.Entry<String,String> entry: s1.entrySet()){
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
 
 
     }
 
     private static void integer_integer_map(){
-        HashMap <Integer, Integer> s1 = new HashMap<>();
-        s1.put(1,100);
-        s1.put(2,200);
-        s1.put(3,300);
+        HashMap<Integer,Integer> s1 = new HashMap<>();
+        s1.put(1,1);
+        s1.put(2,2);
+
+        System.out.println(s1);
+
 
         for (Map.Entry<Integer,Integer> entry: s1.entrySet()){
             System.out.println(entry.getKey()+" "+entry.getValue());
-
         }
 
 
     }
 
     private static void string_integer_map(){
+        HashMap<String,Integer> s1 = new HashMap<>();
+        s1.put("Amit",1);
+        s1.put("Manoj",2);
 
-        HashMap<String, Integer> s1 = new HashMap<>();
-        s1.put("Amit1",100);
-        s1.put("Amit2",1000);
-        s1.put("Amit3",10000);
+        System.out.println(s1);
 
 
         for (Map.Entry<String,Integer> entry: s1.entrySet()){
@@ -43,18 +47,20 @@ public class HelloMap {
         }
 
 
+
     }
 
     private static void integer_string_map(){
-        HashMap<Integer, String> s1 = new HashMap<>();
-        s1.put(100,"Amit");
-        s1.put(1000,"Manoj");
-        s1.put(10000,"Amit3");
+        HashMap<Integer,String> s1 = new HashMap<>();
+        s1.put(1,"Manoj");
+        s1.put(2,"Sunita");
 
+        System.out.println(s1);
 
         for (Map.Entry<Integer,String> entry: s1.entrySet()){
             System.out.println(entry.getKey()+" "+entry.getValue());
         }
+
 
     }
 
