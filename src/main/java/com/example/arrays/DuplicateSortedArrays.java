3d1a35b5c34590d86fd5e394df4890528e334a78
Amit Raj <amit.894@gmail.com>
2022-04-27 23:51:10 +0530

    One more

---
+++ src/main/java/com/example/arrays/DuplicateSortedArrays.java
@@ -0,0 +1,29 @@
+package com.example.arrays;
+
+public class DuplicateSortedArrays {
+
+    private static int removeDuplicates(int [] nums){
+        int unique_index=0;
+        int duplicate_index=1;
+        while (unique_index<nums.length && duplicate_index<nums.length){
+            if (nums[unique_index]==nums[duplicate_index]){
+                duplicate_index+=1;
+            }
+            else {
+                unique_index+=1;
+                int temp=nums[unique_index];
+                nums[unique_index]=nums[duplicate_index];
+                nums[duplicate_index]=temp;
+                duplicate_index+=1;
+            }
+
+        }
+
+        return unique_index+1;
+    }
+
+    public static void main (String args[]){
+        int [] A = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(A));
+    }
+}
