a61caebcc731b93a4164e6d3843fa7b835f1c91b
Amit Raj <amit.894@gmail.com>
2022-05-03 20:52:27 +0530

    Two more

--- src/main/java/com/example/arrays/subArraySumEqualsK.java
+++ src/main/java/com/example/arrays/subArraySumEqualsK.java
@@ -10,30 +10,25 @@ public class subArraySumEqualsK {
             int sum=0;
             for (int j=i; j<nums.length;j++){
                 sum+=nums[j];
-
-                if (sum==k){
+                if (sum==k)
                     count+=1;
-                }
-
             }
-
         }
 
         return count;
     }
 
     private static int SumEqualK1(int [] nums, int k){
-        int count=0;
+        int count =0;
         int sum=0;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map= new HashMap<>();
         map.put(0,1);
         for (int i=0; i<nums.length;i++){
             sum+=nums[i];
-            if (map.containsKey(sum-k)){
+            if (map.containsKey(sum-k))
                 count+=map.get(sum-k);
-            }
-            if (map.containsKey(sum)){
-                map.put(sum,map.get(sum)+1);
+            if(map.containsKey(sum)){
+                map.put(sum, map.get(sum)+1);
             }
             else {
                 map.put(sum,1);
@@ -51,6 +46,9 @@ public class subArraySumEqualsK {
         int [] B = {1,2,3};
 
         System.out.println(SumEqualK1(A,2));
+        System.out.println(SumEqualK1(B,3));
+
+        System.out.println(SumEqualK(A,2));
         System.out.println(SumEqualK1(B,3));
 
     }
