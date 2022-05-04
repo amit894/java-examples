b2480862e469aa7e32d44be25f4e47394d402185
Amit Raj <amit.894@gmail.com>
2022-05-04 14:01:03 +0530

    During the lunch break

--- src/main/java/com/example/arrays/subArraySumEqualsK.java
+++ src/main/java/com/example/arrays/subArraySumEqualsK.java
@@ -5,36 +5,37 @@ import java.util.HashMap;
 public class subArraySumEqualsK {
 
     private static int SumEqualK(int [] nums, int k){
-        int count =0;
-        for (int i=0; i<nums.length;i++){
+        int count = 0;
+        for ( int i=0;i<nums.length;i++){
             int sum=0;
-            for (int j=i; j<nums.length;j++){
+            for ( int j=i;j<nums.length;j++){
                 sum+=nums[j];
-                if (sum==k)
+                if (sum==k){
                     count+=1;
+                }
             }
-        }
 
+        }
         return count;
     }
 
     private static int SumEqualK1(int [] nums, int k){
         int count =0;
-        int sum=0;
-        HashMap<Integer,Integer> map= new HashMap<>();
+        int sum = 0;
+        HashMap <Integer, Integer> map = new HashMap<>();
         map.put(0,1);
-        for (int i=0; i<nums.length;i++){
+        for ( int i=0; i<nums.length;i++){
             sum+=nums[i];
-            if (map.containsKey(sum-k))
+            if (map.containsKey(sum-k)){
                 count+=map.get(sum-k);
-            if(map.containsKey(sum)){
-                map.put(sum, map.get(sum)+1);
+            }
+            if (map.containsKey(sum)){
+                map.put(sum,map.get(sum)+1);
             }
             else {
                 map.put(sum,1);
             }
         }
-
         return count;
 
     }
@@ -44,12 +45,20 @@ public class subArraySumEqualsK {
     public static void main (String args []){
         int [] A = {1,1,1};
         int [] B = {1,2,3};
+        int [] C = {1,2,3};
 
         System.out.println(SumEqualK1(A,2));
         System.out.println(SumEqualK1(B,3));
+        System.out.println(SumEqualK1(C,6));
+        System.out.println(SumEqualK1(C,7));
+
 
         System.out.println(SumEqualK(A,2));
-        System.out.println(SumEqualK1(B,3));
+        System.out.println(SumEqualK(B,3));
+        System.out.println(SumEqualK(C,6));
+        System.out.println(SumEqualK(C,7));
+
+
 
     }
 
--- src/main/java/com/example/strings/validPalindrome.java
+++ src/main/java/com/example/strings/validPalindrome.java
@@ -3,29 +3,41 @@ package com.example.strings;
 public class validPalindrome {
 
     private static boolean isPalindrome(String s, int left,int right){
-        while (left<right){
+        boolean flag = false;
+
+        while (left<=right){
             if (s.charAt(left)!=s.charAt(right))
                 return false;
-            left+=1;
-            right-=1;
+            else {
+                flag = true;
+                left+=1;
+                right-=1;
+            }
+
         }
 
-        return true;
+
+        return flag;
 
     }
 
     public static boolean validPalindrome(String s) {
         int left=0;
         int right=s.length()-1;
+        boolean flag = false;
 
-        while (left<right){
-            if(s.charAt(left)!=s.charAt(right))
+        while ( left<=right){
+            if (s.charAt(left)!=s.charAt(right))
                 return (isPalindrome(s,left+1,right) || isPalindrome(s,left,right-1));
+
             left+=1;
             right-=1;
+            flag=true;
+
         }
 
-        return true;
+        return flag;
+
 
     }
 
